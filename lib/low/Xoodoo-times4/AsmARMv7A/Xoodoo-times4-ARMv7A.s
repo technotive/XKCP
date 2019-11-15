0c616d538db8ca7cabe38a163441663d37f6caf4
Nevernown <ca.boel@student.avans.nl>
2019-11-15 16:07:08 +0100

    Test

--- lib/low/Xoodoo-times4/AsmARMv7A/Xoodoo-times4-ARMv7A.s
+++ lib/low/Xoodoo-times4/AsmARMv7A/Xoodoo-times4-ARMv7A.s
@@ -169,7 +169,8 @@ Xt4_AddLanesAll_Unaligned_Loop:
   pop       {r4-r7,pc}
 Xt4_AddLanesAll_Full:
   vldm      r1!, {d0-d11}
-  vldm      r1!, {d12-d23}
+  mov       pc, r3
+  vldm      r1, {d12-d23}
   vuzp.32   q0, q3
   vuzp.32   q6, q9
   vtrn.32   q0, q6

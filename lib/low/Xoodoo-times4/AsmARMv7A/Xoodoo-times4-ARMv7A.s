1bfe04b8998652f3546c14e4fbcc9956dc2b93cc
Nevernown <ca.boel@student.avans.nl>
2020-01-13 15:48:45 +0100

    One cycle only.

--- lib/low/Xoodoo-times4/AsmARMv7A/Xoodoo-times4-ARMv7A.s
+++ lib/low/Xoodoo-times4/AsmARMv7A/Xoodoo-times4-ARMv7A.s
@@ -1314,20 +1314,23 @@ Xoodootimes4_PermuteAll_12rounds:
 .global Xooffftimes4_CompressFastLoop
 .type Xooffftimes4_CompressFastLoop, %function
 Xooffftimes4_CompressFastLoop:
-  push      {r4-r9, lr}
+  push      {r4-r8, lr}
   vpush     {d8-d15}
-  mov       r9, #0
+  mov       r14, #0
   sub       r3, #192
 Xft4_CompressFast:
   everest
   xoodoo_6_star
   avalanche
-  add       r9, #192
+  mov r0, r3
+  pop {r4-r8, pc}
+
+  add       r14, #192
   subs      r3, #192
   bhi       Xft4_CompressFast
-  mov       r0, r9
+  mov       r0, r14
   vpop      {d8-d15}
-  pop       {r4-r9, pc}
+  pop       {r4-r8, pc}
 
 @ Xooffftimes4_ExpandFastLoop: uchar * yAccu -> uchar * kRoll -> uchar * output -> size_t length -> size_t
 @ .align 8

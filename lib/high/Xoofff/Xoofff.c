7e1e059748077524c701948184711812f71fc28e
Nevernown <ca.boel@student.avans.nl>
2020-01-18 13:12:39 +0100

    Removed output.

--- lib/high/Xoofff/Xoofff.c
+++ lib/high/Xoofff/Xoofff.c
@@ -487,10 +487,6 @@ int Xoofff_Compress(Xoofff_Instance *xp, const BitSequence *input, BitLength inp
         }
     }
     if ( (inputBitLen >= SnP_width) || (finalFlag != 0) ) { /* Compress blocks */
-      if(inputBitLen/8 > 192 && ((uint32_t)input)%4 != 0) {
-        printf("\nInput:%u - R3:%u\n", input, inputBitLen/8);
-      }
-
         input = Xoodoo_CompressBlocks(xp->kRoll.a, xp->xAccu.a, input, &inputBitLen, finalFlag);
     }
     if ( inputBitLen != 0 ) { /* Queue eventual residual message bytes */

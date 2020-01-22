c4c21aaf9dea4d716e1fcef8addc620ff88055ab
Nevernown <ca.boel@student.avans.nl>
2020-01-22 11:41:34 +0100

    Better output

--- tests/UnitTests/testXoofff.c
+++ tests/UnitTests/testXoofff.c
@@ -256,9 +256,10 @@ void tempTest(){
   }
 
   Xooffftimes4_ExpandFastLoop((unsigned char*) in, (unsigned char*) k, (unsigned char*) out, 48*4);
-
+  printf("Compression:\n");
   for(i = 0; i < 48; i++) {
     if(i % 12 == 0) { printf("\n"); }
+    printf("%3u:", i);
     printf("%3u ", out[i]);
   }
 }

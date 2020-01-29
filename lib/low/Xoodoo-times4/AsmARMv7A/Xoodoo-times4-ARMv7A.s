5ce80b6e7ba5e295c7bb6d2380ecd67a9f021011
Nevernown <ca.boel@student.avans.nl>
2020-01-29 17:02:56 +0100

    Do not perform last loop check.

--- lib/low/Xoodoo-times4/AsmARMv7A/Xoodoo-times4-ARMv7A.s
+++ lib/low/Xoodoo-times4/AsmARMv7A/Xoodoo-times4-ARMv7A.s
@@ -1573,6 +1573,7 @@ Xft4_ExpandFast:
   sequentiate
   add       r11, #192
   subs      r3, #192
+  subs      r3, #192 @ Do not perform last loop.
   bcs       Xft4_ExpandFast
   mov       r0, r11
   vpop      {d8-d15}

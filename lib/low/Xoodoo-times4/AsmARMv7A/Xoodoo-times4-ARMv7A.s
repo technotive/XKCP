6247c2a29a7233f37495a0eaf01d6eac4eed02f7
Nevernown <ca.boel@student.avans.nl>
2020-03-04 13:47:47 +0100

    Experimental VEXT in rho_west_column.6

--- lib/low/Xoodoo-times4/AsmARMv7A/Xoodoo-times4-ARMv7A.s
+++ lib/low/Xoodoo-times4/AsmARMv7A/Xoodoo-times4-ARMv7A.s
@@ -1152,7 +1152,7 @@ Xft4_AddIs_0:
   @ vext.32   d0, d11, d10, #1
   @ vext.32   d1, d10, d11, #1
 
-  vext.32   q0, q5, q5, #1
+  vext.32   q0, q5, q5, #3
 
   vmov.32   r5, r6, d16
   vmov.32   r7, r8, d17

967d1688baa5091a9ced62f268ce3f630b21f5b4
Nevernown <ca.boel@student.avans.nl>
2020-03-04 13:05:34 +0100

    revert.1

--- lib/low/Xoodoo-times4/AsmARMv7A/Xoodoo-times4-ARMv7A.s
+++ lib/low/Xoodoo-times4/AsmARMv7A/Xoodoo-times4-ARMv7A.s
@@ -1190,9 +1190,9 @@ Xft4_AddIs_0:
 .endm
 
 .macro iota_column
-  @ mov       r8, #0
+  mov       r8, #0
   vmov      s24, r7
-  vmov.32   s25, #0
+  vmov.32   s25, r8
   veor      d8, d8, d12
   veor      d14, d14, d12
   veor      d20, d20, d12
